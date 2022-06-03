00aa28b6105dcf2c1030bf0ad822321bb3f37d9b
Kami Isaacs <6ix5ini@Kamis-Mac-mini-4.local>
2022-06-03 19:39:25 -0400

    working kode

--- climbStairs.js
+++ climbStairs.js
@@ -1,5 +1,13 @@
 function climbStairs(n){
-    return n
-};
+   let  store = [];
+    if(n ==1 || n ==2){
+        return n
+    }else if(store[n] == true){
+        return store[n];
 
+    }else{
+        let store[n] =
+    }
+    
+}
 console.log(climbStairs(2))
